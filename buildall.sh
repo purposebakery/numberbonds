e1096eb66e87e4986d34f61f6b6fabf549976dc7
Oliver Metz <ometz@posteo.de>
2021-06-28 22:35:29 +1200

    android to minSDK 19 + some documentation

--- buildall.sh
+++ buildall.sh
@@ -9,17 +9,21 @@ echo "Test results ${TEST_RESULTS}"
 # Android
 echo '=== BUILDING ANDROID ==='
 flutter build appbundle --release
-# RELEASE: upload build/app/outputs/bundle/release/app-release.aab to play store
+# RELEASE:
+# upload build/app/outputs/bundle/release/app-release.aab to play store
 
 # iOS
 echo '=== BUILDING iOS ==='
 flutter build ipa --release
-# RELEASE: open build/ios/archive/Runner.xcarchive -> verify -> distribute
+# RELEASE:
+# > open build/ios/archive/Runner.xcarchive
+# -> verify -> distribute
 
 # Web
 echo '=== BUILDING WEB ==='
 flutter build web --release
-# RELEASE: 
+# RELEASE:
+# > cp -R build/web/* ../githubio/numberbonds/
 # > cd ../githubio/numberbonds
 # > git commit -a -m "release"
 # > git push
